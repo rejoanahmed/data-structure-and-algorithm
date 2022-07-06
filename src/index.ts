ffa28dc2648b7e7b328a9d3d58cd80e6f1d4fde8
rejoanahmed <87889021+rejoanahmed@users.noreply.github.com>
2022-07-06 10:04:33 +0600

    iterative approach

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 function fib(n: number): number {
-  if (n === 0) return 0;
-  if (n === 1 || n === 2) return 1;
-
-  return fib(n - 1) + fib(n - 2);
+  if (n < 2) return n;
+  let pv1 = 1,
+    pv2 = 0,
+    tmp;
+  for (let i = 2; i < n; i++) {
+    tmp = pv1 + pv2;
+    pv2 = pv1;
+    pv1 = tmp;
+  }
+  return pv1 + pv2;
 }
 console.log(fib(7));
